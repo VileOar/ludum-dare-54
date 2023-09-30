2d313de7aed2ff44d060487d8e3d19621cbcc2f2
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-09-30 16:10:51 +0100

    Squashed commit of the following:
    commit 0a64fb43174ff5db4a88c6aea1a8a81a178bcd7d
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 16:08:33 2023 +0100
    
        Squashed commit of the following:
    
        commit ade0938cafda3006beb30f0c38378cac5b483971
        Author: PMSValerio <37459155+PMSValerio@users.noreply.github.com>
        Date:   Sat Sep 30 12:36:14 2023 +0100
    
            added basic space managing
    
        commit a3c708c0caebfec44bb9daeb5089da51e46b540c
        Author: PMSValerio <37459155+PMSValerio@users.noreply.github.com>
        Date:   Sat Sep 30 11:30:36 2023 +0100
    
            added file_spawner
    
    commit 8d11dedda2027c6d6f0268aedfd8573d4dcdd1be
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 15:59:18 2023 +0100
    
        Release multi-select + clamp file position
    
    commit 82ba43325cec27d00f4203de70c9bc34bceba4f7
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 15:48:08 2023 +0100
    
        Multiselect working (i guess)
    
    commit 7466abd143af2510c98647c34f992cc99b8c05c7
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 12:30:54 2023 +0100
    
        Added TODO
    
    commit 04a774d0b43ef7dd896ccfe3fbdd239f0b38b7d6
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 12:29:37 2023 +0100
    
        Selected files to desketop
    
    commit 4823b9ef8c604a8837113dde8e3b25adc950054b
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 11:34:09 2023 +0100
    
        Merge
    
    commit d2fcbabd689f7047c323930cb4ab5365a7e32eca
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 11:31:43 2023 +0100
    
        Squashed commit of the following:
    
        commit a3c708c0caebfec44bb9daeb5089da51e46b540c
        Author: PMSValerio <37459155+PMSValerio@users.noreply.github.com>
        Date:   Sat Sep 30 11:30:36 2023 +0100
    
            added file_spawner
    
    commit e5b851c40c564373521211c7ece16787a14be2fa
    Author: Lucius Vinicius <luciusviniciusf@ua.pt>
    Date:   Sat Sep 30 11:10:44 2023 +0100
    
        Offset + base area for multiselect

--- scripts/autoloads/global.gd
+++ scripts/autoloads/global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var selected_files : Array = []
 
 func _ready():
 	randomize()
--- scripts/draggable_file.gd
+++ scripts/draggable_file.gd
@@ -3,6 +3,13 @@ extends Node2D
 class_name DraggableFile
 
 var lifted := false
+var mouse_offset := Vector2.ZERO
+@onready var rect = get_viewport_rect()
+@onready var xx = rect.size.x/2 - 20
+@onready var yy = rect.size.y/2 - 20
+
+## default file size
+var file_size := 5
 
 ## default file size
 var file_size := 5
@@ -15,7 +22,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if lifted:
-		position = get_global_mouse_position()
+		position = get_global_mouse_position() + mouse_offset
+		position.x = clamp(position.x, -xx, xx)
+		position.y = clamp(position.y, -yy, yy)
 
 
 func _on_color_rect_gui_input(event):
@@ -23,11 +32,27 @@ func _on_color_rect_gui_input(event):
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
 				if event.pressed:
-					lifted = true
-					# Put file on "top" of parent children
-					var parent = get_parent()
-					get_parent().move_child(self, -1)
+					# Multiple Selection
+					if Global.selected_files.is_empty():
+						set_lifted(true)
+					else:
+						for file in Global.selected_files:
+							file.set_lifted(true)
 				else:
-					lifted = false
-					SignalManager.release_file.emit(self)
+					# Multiple Selection
+					if Global.selected_files.is_empty():
+						set_lifted(false)
+						SignalManager.release_file.emit(self)
+					else:
+						for file in Global.selected_files:
+							file.set_lifted(false)
+						SignalManager.release_files.emit(Global.selected_files)
 			# RIGHT CLICK HANDLER GOES HERE
+
+func set_lifted(val : bool):
+	lifted = val
+	if not val: return
+	mouse_offset = position - get_global_mouse_position()
+	# Put file on "top" of parent children
+	var parent = get_parent()
+	get_parent().move_child(self, -1)
