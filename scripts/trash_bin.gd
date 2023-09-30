2e9c3018b4c8a5451d58872804437f7bd616e176
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-09-30 10:37:34 +0100

    I forgor trash position condition, oopsie doopsie

--- scripts/trash_bin.gd
+++ scripts/trash_bin.gd
@@ -1,10 +1,18 @@
 extends DraggableFile
 
+var mouse_hovered := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalManager.release_file.connect(remove_file)
 
 func remove_file(file : Node2D):
-	if file.name == "TrashBin": return
+	if file.name == "TrashBin" or not mouse_hovered: return
 	file.queue_free()
+
+func _on_area_2d_mouse_entered():
+	mouse_hovered = true
+
+
+func _on_area_2d_mouse_exited():
+	mouse_hovered = false
