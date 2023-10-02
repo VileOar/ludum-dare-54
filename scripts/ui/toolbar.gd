cc0cbf5b5b08d73c65bf0fa8b4a4bbf825838984
Franciscorp <franciscorp1994@hotmail.com>
2023-10-02 01:02:07 +0100

    Click and Pause with music
    [Pause with music] Now music continues on pause
    [Click sfx] Toolbar nowp lays click sfx

--- scripts/ui/toolbar.gd
+++ scripts/ui/toolbar.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name Toolbar
 
+@onready var _click_sfx := $ClickSFX
+
 @onready var disk_space_bar := %DiskSpaceBar
 @onready var disk_space_label := %DiskSpaceLabel
 @onready var pause_menu := %PauseMenu
@@ -21,16 +23,19 @@ func _on_disk_space_manager_space_update(new_space, max_space):
 	
 	disk_space_label.text = str(max_space - new_space) + " free out of " + str(max_space)
 
+func _play_click_sfx():
+	_click_sfx.play()
 
 func _on_empty_button_pressed():
+	_play_click_sfx()
 	SignalManager.empty_trash.emit()
 
-
 func _on_anti_v_button_pressed():
+	_play_click_sfx()
 	SignalManager.toggle_antivirus.emit()
 
-
 func _on_start_button_pressed():
+	_play_click_sfx()
 	if get_tree().paused:
 		get_tree().paused = false
 		pause_menu.hide()
@@ -42,9 +47,12 @@ func _on_start_button_pressed():
 
 
 func _on_logoff_button_pressed():
+	_play_click_sfx()
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_shutdown_button_pressed():
+	_play_click_sfx()
 	get_tree().quit()
+	
