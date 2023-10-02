5154007699bffa0120062e4fa999e08fa6883c76
Franciscorp <franciscorp1994@hotmail.com>
2023-10-02 19:10:27 +0100

    Fix music bug

--- scripts/audio/game_audio.gd
+++ scripts/audio/game_audio.gd
@@ -3,8 +3,10 @@ extends Node
 @onready var _defeat_sfx = $DefeatSFX
 @onready var _music_audio : AudioStreamPlayer = $MusicPlayer
 @onready var _panic_music_audio : AudioStreamPlayer = $PanicMusicPlayer
+
 @onready var _timer : Timer = $Timer
 @onready var _is_disk_at_warning_level := false
+@onready var _audio_fades_animation := $AnimationPlayer
 
 @export var transition_duration : float = 6.00
 @export var time_till_music_starts : float = 3.00
@@ -14,6 +16,8 @@ func _ready():
 	SignalManager.disk_almost_full.connect(_on_disk_almost_full)
 	SignalManager.play_game_over_sfx.connect(_game_over)
 	fade_in(_music_audio, 1.00)
+	_panic_music_audio.play()
+	_panic_music_audio.volume_db = UtilsAudio.MIN_VOLUME
 	_timer.wait_time = time_till_music_starts
 
 # TODO delete. it's not used anymore
@@ -31,16 +35,18 @@ func _on_disk_almost_full(is_disk_almost_full):
 	# if disk is playing panic song and no longer is almost full. fades out
 	if _is_disk_at_warning_level:
 		if !is_disk_almost_full:
+			_audio_fades_animation.play("change_to_normal")
 #			print("warning = true | back to normal music")
-			fade_out(_panic_music_audio, transition_duration)
-			fade_in(_music_audio, time_till_music_starts)
+#			fade_out(_panic_music_audio, transition_duration)
+#			fade_in(_music_audio, time_till_music_starts)
 			_is_disk_at_warning_level = false
 	
 	if !_is_disk_at_warning_level:
 		if is_disk_almost_full:
+			_audio_fades_animation.play("change_to_panic")
 #			print("warning = false | panic music")
-			fade_in(_panic_music_audio, time_till_music_starts)
-			fade_out(_music_audio, transition_duration)
+#			fade_in(_panic_music_audio, time_till_music_starts)
+#			fade_out(_music_audio, transition_duration)
 			_is_disk_at_warning_level = true
 
 
@@ -58,7 +64,4 @@ func fade_in(stream_player, duration):
 	_tween_in.tween_property(stream_player, "volume_db", UtilsAudio.DEFAULT_VOLUME, duration)
 	# when the tween ends, the music will be stopped
 	
-# stop the music -- otherwise it continues to run at silent volume
-func _on_TweenOut_tween_completed(object, _key):
-	print("fade out completed")
-	object.stop()
+
