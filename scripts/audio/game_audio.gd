75e82c259bbef52eb56b2939519cd120059ce29e
Franciscorp <franciscorp1994@hotmail.com>
2023-10-02 19:22:05 +0100

    Cleanup

--- scripts/audio/game_audio.gd
+++ scripts/audio/game_audio.gd
@@ -4,12 +4,8 @@ extends Node
 @onready var _music_audio : AudioStreamPlayer = $MusicPlayer
 @onready var _panic_music_audio : AudioStreamPlayer = $PanicMusicPlayer
 
-@onready var _timer : Timer = $Timer
 @onready var _is_disk_at_warning_level := false
 @onready var _audio_fades_animation := $AnimationPlayer
-
-@export var transition_duration : float = 6.00
-@export var time_till_music_starts : float = 3.00
 
 
 func _ready():
@@ -18,11 +14,7 @@ func _ready():
 	fade_in(_music_audio, 1.00)
 	_panic_music_audio.play()
 	_panic_music_audio.volume_db = UtilsAudio.MIN_VOLUME
-	_timer.wait_time = time_till_music_starts
 
-# TODO delete. it's not used anymore
-func _on_timer_timeout():
-	fade_out(_music_audio, transition_duration)
 
 # Signal comes frmo disk toolbar
 func _game_over():
@@ -37,16 +29,12 @@ func _on_disk_almost_full(is_disk_almost_full):
 		if !is_disk_almost_full:
 			_audio_fades_animation.play("change_to_normal")
 #			print("warning = true | back to normal music")
-#			fade_out(_panic_music_audio, transition_duration)
-#			fade_in(_music_audio, time_till_music_starts)
 			_is_disk_at_warning_level = false
 	
 	if !_is_disk_at_warning_level:
 		if is_disk_almost_full:
 			_audio_fades_animation.play("change_to_panic")
 #			print("warning = false | panic music")
-#			fade_in(_panic_music_audio, time_till_music_starts)
-#			fade_out(_music_audio, transition_duration)
 			_is_disk_at_warning_level = true
 
 
