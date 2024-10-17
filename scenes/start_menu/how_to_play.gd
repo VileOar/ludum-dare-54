181390bc86f396d6e32bee4f51964713d93f4d5e
PMSValerio <37459155+VileOar@users.noreply.github.com>
2024-10-17 17:01:57 +0100

    [WIP] refactor main menu

---
+++ scenes/start_menu/how_to_play.gd
@@ -0,0 +1,35 @@
+class_name HowToPlay
+extends MarginContainer
+## Screen for controlling the how to play interactions
+
+@onready var _tab_buttons = %TabButtons
+
+@onready var _screen_container = %ScreenContainer
+@onready var _objectives = %Objectives
+@onready var _corrupted = %Corrupted
+@onready var _antivirus = %Antivirus
+
+
+var _tab_btn_group: ButtonGroup
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_tab_btn_group = ButtonGroup.new()
+	_tab_btn_group.allow_unpress = false
+	for btn in _tab_buttons.get_children():
+		btn = btn as Button
+		btn.button_group = _tab_btn_group
+	var _v = _tab_btn_group.pressed.connect(_on_tab_changed)
+
+
+func _on_tab_changed(button: BaseButton):
+	for node in _screen_container.get_children():
+		node.hide()
+	match button.name:
+		"BtnObjectives":
+			_objectives.show()
+		"BtnCorrupted":
+			_corrupted.show()
+		"BtnAntivirus":
+			_antivirus.show()
---
+++ scenes/start_menu/main_menu.gd
@@ -0,0 +1,51 @@
+extends Control
+
+
+@export var main_scene : PackedScene
+
+@onready var _menu_audio = $MenuAudio
+
+@onready var _main_buttons = %Buttons
+@onready var _backable_screens = %BackableScreens
+
+@onready var _screen_holder = %ScreenHolder
+@onready var _credits = %Credits
+@onready var _how_to_play = %HowToPlay
+
+
+func _ready():
+	for btn in find_children("*", "BaseButton"):
+		(btn as BaseButton).mouse_entered.connect(_on_btn_mouse_entered)
+
+
+func _on_btn_mouse_entered():
+	_menu_audio.play_hover_sfx()
+
+
+func _on_back_pressed():
+	_backable_screens.hide()
+	_main_buttons.show()
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_packed(main_scene)
+
+
+func _on_how_to_play_pressed():
+	_main_buttons.hide()
+	_backable_screens.show()
+	for node in _screen_holder.get_children():
+		node.hide()
+	_how_to_play.show()
+
+
+func _on_credits_pressed():
+	_main_buttons.hide()
+	_backable_screens.show()
+	for node in _screen_holder.get_children():
+		node.hide()
+	_credits.show()
+
+
+func _on_exit_pressed():
+	get_tree().quit()
